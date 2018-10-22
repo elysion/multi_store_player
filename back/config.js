83379aaf8e7b4041836de23dbea3cbf0d7fc94b1
Miko Kiiski <miko.kiiski@gmail.com>
2018-10-23 00:18:47 +0300

    Add config for dev frontend to pm2 config

--- back/config.js
+++ back/config.js
@@ -8,6 +8,6 @@ const currentIp = getIPv4AddressOfInterface('en0')
 console.log('Current IP: ', currentIp)
 
 module.exports = {
-  allowedOrigins: ['http://localhost:4001', `http://${currentIp}:4001`],
+  allowedOrigins: ['http://localhost:4001', `http://${currentIp}:4001`, 'http://localhost:5001', `http://${currentIp}:5001`],
   port: 4000
 }
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,8 +18,19 @@ module.exports = {
       }
     },
     {
-      name: 'frontend (4001)',
+      name: 'prod frontend (4001)',
       script: 'npm run start:prod',
+      cwd: `${__dirname}/front`,
+      log_file: 'frontend.prod.log',
+      pid_file: '.frontend.prod.pm2.pid',
+      combine_logs: true,
+      restart_delay: 4000,
+      wait_ready: true,
+      watch: false
+    },
+    {
+      name: 'frontend (5001)',
+      script: 'npm run start',
       cwd: `${__dirname}/front`,
       log_file: 'frontend.log',
       pid_file: '.frontend.pm2.pid',
