bf8eea548d86397f176d0ff7671d62e32b658ab9
Miko Kiiski <miko.kiiski@reaktor.fi>
2018-10-27 22:21:37 +0300

    Implement a test for merging artists on insert

--- back/env.js
+++ back/env.js
@@ -1,6 +1,15 @@
-module.exports = {
-  dbUrl: "postgres://localhost/multi-store-player",
-  statementTimeout: "60s",
-  sessionKey: "multi player secret",
-  port: 4000
+const envs = {
+  dev: {
+    dbUrl: 'postgres://localhost/multi-store-player',
+    statementTimeout: '60s',
+    sessionKey: 'multi player secret',
+    port: 4000
+  },
+  test: {
+    dbUrl: 'postgres://localhost/multi-store-player-test',
+    statementTimeout: '60s',
+    sessionKey: 'multi player secret',
+    port: 4000
+  }
 }
+module.exports = envs[process.env.NODE_ENV || 'dev']
