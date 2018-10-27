ab3cedfcd6501f9b54558d610069cacd25ad4fae
Miko Kiiski <miko.kiiski@reaktor.fi>
2018-10-27 13:56:10 +0300

    Replace manual migrations with db-migrate

--- back/migrations/sqls/20181027103351-init-up.sql
+++ back/migrations/sqls/20181027103351-init-up.sql
@@ -1,26 +1,12 @@
+-- begin;
+
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-ALTER DATABASE :"DBNAME" SET time ZONE 'Europe/Helsinki';
+ALTER DATABASE "multi-store-player" SET time ZONE 'Europe/Helsinki';
 
 CREATE TYPE TRACK__ARTIST_ROLE AS ENUM ('author', 'remixer');
 CREATE TYPE PREVIEW_FORMAT AS ENUM ('mp3', 'mp4');
 
-DROP TABLE IF EXISTS meta_account CASCADE;
-
-DROP TABLE IF EXISTS meta_session CASCADE;
-DROP TABLE IF EXISTS store CASCADE;
-DROP TABLE IF EXISTS artist CASCADE;
-DROP TABLE IF EXISTS track CASCADE;
-DROP TABLE IF EXISTS label CASCADE;
-DROP TABLE IF EXISTS track__label CASCADE;
-DROP TABLE IF EXISTS store__artist CASCADE;
-DROP TABLE IF EXISTS store__track CASCADE;
-DROP TABLE IF EXISTS store__label CASCADE;
-DROP TABLE IF EXISTS user__track CASCADE;
-DROP TABLE IF EXISTS track__artist CASCADE;
-DROP TABLE IF EXISTS store__track_preview CASCADE;
-DROP TABLE IF EXISTS user__artist__label_ignore CASCADE;
-
-CREATE TABLE IF NOT EXISTS meta_account (
+CREATE TABLE meta_account (
   meta_account_user_id  SERIAL PRIMARY KEY,
   meta_account_username VARCHAR(50)  NOT NULL UNIQUE, -- email?
   meta_account_details  JSONB        NOT NULL DEFAULT '{}' :: JSONB,
@@ -48,9 +34,9 @@ CREATE TABLE IF NOT EXISTS artist (-- TODO: how to differentiate artists with sa
 );
 
 CREATE TABLE IF NOT EXISTS track (
-  track_id         SERIAL PRIMARY KEY,
-  track_title      VARCHAR(100)             NOT NULL,
-  track_added      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+  track_id    SERIAL PRIMARY KEY,
+  track_title VARCHAR(100)             NOT NULL,
+  track_added TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS track__artist (
@@ -86,7 +72,6 @@ CREATE TABLE IF NOT EXISTS store__track (
   track_id                   INTEGER REFERENCES track (track_id)   NOT NULL,
   store_id                   INTEGER REFERENCES store (store_id)   NOT NULL,
   store__track_store_id      TEXT UNIQUE                           NOT NULL,
-  store__track_released      DATE,
   store__track_store_details JSONB                                 NOT NULL
 );
 
@@ -109,16 +94,9 @@ CREATE TABLE IF NOT EXISTS user__track (
   user__track_id       SERIAL PRIMARY KEY,
   track_id             INTEGER REFERENCES track (track_id),
   meta_account_user_id INTEGER REFERENCES meta_account (meta_account_user_id),
-  user__track_heard    timestamptz,
+  user__track_heard    boolean,
   UNIQUE (track_id, meta_account_user_id)
 );
-
--- is this needed? an artist already relates to a label through artist__track
--- CREATE TABLE artist__label (
---   artist__label_id SERIAL PRIMARY KEY,
---   artist_id        INTEGER REFERENCES artist (artist_id),
---   label_id         INTEGER REFERENCES label (label_id)
--- );
 
 CREATE TABLE user__artist__label_ignore (
   user__artist__label_ignore_id SERIAL PRIMARY KEY,
@@ -127,11 +105,6 @@ CREATE TABLE user__artist__label_ignore (
   label_id                      INTEGER REFERENCES label (label_id)
 );
 
--- CREATE TABLE IF NOT EXISTS meta_store_session (
---   meta_store_session_id      SERIAL PRIMARY KEY,
---   store_id                   INTEGER REFERENCES store (store_id)                     NOT NULL,
---   meta_session_is            INTEGER REFERENCES meta_session (meta_store_session_id) NOT NULL,
---   meta_store_session_session JSONB                                                   NOT NULL
--- );
-
 INSERT INTO store (store_name) VALUES ('Beatport');
+
+commit;
