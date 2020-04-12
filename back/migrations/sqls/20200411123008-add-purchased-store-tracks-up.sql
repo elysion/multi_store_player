fc36fc6953403f006cebf9ab09c5a0cfc120d7ce
Miko Kiiski <miko.kiiski@reaktor.fi>
2020-04-12 18:26:39 +0300

    Fix purchased track db migration script syntax error

--- back/migrations/sqls/20200411123008-add-purchased-store-tracks-up.sql
+++ back/migrations/sqls/20200411123008-add-purchased-store-tracks-up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user__store__track_purchased (
   meta_account_user_id INTEGER REFERENCES meta_account (meta_account_user_id)   NOT NULL,
-  store__track_id      INTEGER REFERENCES store__track (store__track_id)        NOT NULL
+  store__track_id      INTEGER REFERENCES store__track (store__track_id)        NOT NULL,
   UNIQUE (meta_account_user_id, store__track_id)
 );
